528d39d33eb4ebf65c75bf5f95914fc0c895b5d1
Janos Jarecsni <janos.jarecsni@gmail.com>
2019-08-03 18:49:51 +0100

    Refactor tests

--- appointments/Appointments.test.js
+++ appointments/Appointments.test.js
@@ -7,14 +7,17 @@ describe('Appointments', () => {
     beforeEach(() => {
         container = document.createElement('div');
     });
+
+    const render = component => ReactDOM.render(component, container);
+
     it('renders the customer first name', () => {
         customer = { firstName: 'Ashley'};
-        ReactDOM.render(<Appointment customer={customer}/>, container);
+        render(<Appointment customer={customer}/>);
         expect(container.textContent).toMatch('Ashley');
     });
     it('renders another customer first name', () => {
         customer = { firstName: 'Jordan'};
-        ReactDOM.render(<Appointment customer={customer}/>, container);
+        render(<Appointment customer={customer}/>);
         expect(container.textContent).toMatch('Jordan');
    });
 });
