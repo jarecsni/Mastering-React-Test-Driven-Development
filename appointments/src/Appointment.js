8d1c701c3c49582cd6c52aed79de2c1d38520bb0
Janos Jarecsni <janos.jarecsni@gmail.com>
2019-08-03 20:29:40 +0100

    To page 54.

--- appointments/src/Appointment.js
+++ appointments/src/Appointment.js
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export const Appointment = ({ customer }) => (
   <div>{customer.firstName}</div>
 );
+
+const appointmentTimeOfDay = time => {
+  const [h, m] = new Date(time).toTimeString().split(':');
+  return `${h}:${m}`;
+}
+
+export const AppointmentsDayView = ({appointments}) => {
+  const [selectedAppointment, setSelectedAppointment] = useState(0);
+  return (
+    <div id="appointmentsDayView">
+      <ol>
+      {appointments.map((appointment, i) => (
+        <li key={appointment.startsAt}>
+          <button type="button" onClick={()=>{setSelectedAppointment(i)}}>
+            {appointmentTimeOfDay(appointment.startsAt)}
+          </button>
+        </li>
+      ))}
+      </ol>
+      { appointments.length === 0 ? (
+        <p>There are no appointments scheduled for today.</p>
+      ) : (
+        <Appointment {...appointments[selectedAppointment]} />
+      )}
+    </div>
+  );
+}
+
+
