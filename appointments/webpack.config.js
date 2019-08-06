793375a14a328c08c10580ccef43d6aba629e2c0
Janos Jarecsni <janos.jarecsni@gmail.com>
2019-08-06 07:08:36 +0100

    Page 65

--- appointments/webpack.config.js
+++ appointments/webpack.config.js
@@ -2,10 +2,22 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
- mode: "development",
- module: {
-   rules: [{
-     test: /\.(js|jsx)$/,
-     exclude: /node_modules/,
-     loader: 'babel-loader'}]}
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  mode: 'development',
+  devtool: 'cheap-module-eval-source-map',
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, './dist'),
+    historyApiFallback: true
+  },
 };
